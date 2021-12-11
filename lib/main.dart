d580a1f6ae81840502c662ce15d28815e50645be
Yeasin Arafat <mdarafat1819@gmail.com>
2021-12-11 10:24:47 +0600

    upLeftDice and upRgithDice Button added

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -6,8 +7,12 @@ void main() {
       home: Scaffold(
         backgroundColor: Colors.red,
         appBar: AppBar(
-          title: Text('Ludu Game'),
-          backgroundColor: Colors.red,
+          title: Text(
+            'Ludu Game',
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
+          ),
+          centerTitle: true,
+          backgroundColor: Colors.red,S
         ),
         body: LuduGame(),
       ),
@@ -15,10 +20,50 @@ void main() {
   );
 }
 
-class LuduGame extends StatelessWidget {
-  // This widget is the root of your application.
+class LuduGame extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _LuduGameState();
+  }
+}
+
+class _LuduGameState extends State<LuduGame> {
+  int upLeftDice = Random().nextInt(6) + 1;
+  int upRightDice = Random().nextInt(6) + 1;
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            flex: 1,
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: FlatButton(
+                child: Image.asset('images/chokkha-$upLeftDice.png'),
+                onPressed: () {
+                  upLeftDice = Random().nextInt(6) + 1;
+                  setState(() {});
+                },
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: TextButton(
+                child: Image.asset('images/chokkha-$upRightDice.png'),
+                onPressed: () {
+                  upRightDice = Random().nextInt(6) + 1;
+                  setState(() {});
+                },
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
